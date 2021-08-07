badee0c5132b4a43bf8f286c4af8081084495a18
unknown <d.dogan@etu.edu.tr>
2021-08-07 11:36:50 +0300

    getUserPosts function added

--- src/functions.js
+++ src/functions.js
@@ -1,18 +1,39 @@
-const functions = {
-    divide: (num1,num2) => {
-      if(num2 === 0) {
-           throw new Error('Divide by zero error')
-      }
-      else if(isNaN(num1) || isNaN(num2) ) {
-        throw new Error('Invalid error')
-      }
-      return num1/num2;
-    },
-    toUpper:(title) => {
-      const arr= title.split(" ")
-      const newArr = arr.map(str => str.charAt(0).toUpperCase()+str.slice(1))
-      return newArr.join(" ");
-    }
-};
 
+import axios from "axios";
+const functions = {
+  divide: (num1, num2) => {
+    if (num2 === 0) {
+      throw new Error('Divide by zero error')
+    }
+    else if (isNaN(num1) || isNaN(num2)) {
+      throw new Error('Invalid error')
+    }
+    return num1 / num2;
+  },
+  toUpper: (title) => {
+    const arr = title.split(" ")
+    const newArr = arr.map(str => str.charAt(0).toUpperCase() + str.slice(1))
+    return newArr.join(" ");
+  },
+  getUserPosts() {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        let resp = response.data
+        let items = new Array(11).fill(0);
+        resp.map(item => {
+          return items[item.userId]++;
+        })
+        let max = items[1];
+        for (let i = 2; i <= items.length; i++) {
+          if (items[i] >= max) {
+            max = i
+          }
+        }
+        const titles = resp.filter(item => item.userId === max).map(item => {
+          return item.title
+        })
+        return titles;
+      }).catch(err => 'Error occured');
+  }
+};
 module.exports = functions;
